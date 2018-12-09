1aaf9f24001772a9a091512bb6f94425fc494e19
wh.jin <wh.jin@hotmail.com>
2018-12-09 21:01:21 +0800

    s

--- data_exploration.py
+++ data_exploration.py
@@ -45,17 +45,26 @@ font_size = 5
 # import data
 # 列名字有中文的时候，encoding='utf-8',不然会出错
 # index_col设置属性列，parse_dates设置是否解析拥有时间值的列
+dateparser = lambda x: pd.datetime.strptime(x, '%Y-%m-%d %H:%M:%S')
 data_raw = pd.read_csv(
-    'data/data.csv', sep=',', index_col=0, encoding='utf-8', parse_dates=True)
+    'data/data.csv',
+    sep=',',
+    index_col=0,
+    encoding='utf-8',
+    parse_dates=True,
+    date_parser=dateparser)
 print(data_raw.info())
 # print(data_raw.sample(10))
-
+data_raw.drop([
+    'TNT1-Y太阳电池内板温度1', 'TNT14锂离子蓄电池组A温度1', 'TNT15锂离子蓄电池组A温度2',
+    'TNT4-Y太阳电池内板温度4'],axis=1,inplace=True)
 print('Train columns with null values:\n', data_raw.isnull().sum())
 print("-" * 10)
 
 print(data_raw.describe(include='all'))
 
 data_new = data_raw.dropna()
+data_new.to_csv('data/1208_new.csv', encoding='utf-8')
 """
 绘制多个子图
 一个Figure对象可以包含多个子图（Axes），在matplotlib中用Axes对象表示一个绘图区域，称为子图，可以使用subplot()快速绘制包含多个子图的图表，它的调用形式如下：
@@ -85,7 +94,7 @@ for i, column in enumerate(data_new.columns):
     ax = fig1.add_subplot(len(data_new.columns), 1, i + 1)
     ax.plot(data_raw.loc[:, [column]], 'o', color='green', markersize=0.5)
     ax.xaxis.set_major_formatter(mdate.DateFormatter('%Y-%m-%d %H:%M:%S'))
-    
+
     # plt.axes([0.14, 0.35, 0.77, 0.9])
     # ax.xaxis.set_tick_params(rotation=30)
     ax.set_title(column, FontProperties=font)
@@ -93,6 +102,5 @@ plt.subplots_adjust(
     left=0.02, bottom=0.01, right=0.98, top=0.99, hspace=0.4, wspace=0.3)
 # left=0.02, bottom=0.01, right=0.98, top=0.99 分别代表到画布的左侧和底部的距离占整幅图宽和高的比例
 plt.savefig('result/column_new.png')
-data_new.to_csv('data_new.csv', encoding='utf-8')
 
 print('finish')
